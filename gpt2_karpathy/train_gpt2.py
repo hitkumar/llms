9413da18b9ec5874e6841a243062ffe4e2b0530a
Hitesh Kumar <htkumar@meta.com>
2024-06-22 09:14:44 -0700

    Adding dataloader lite

--- gpt2_karpathy/train_gpt2.py
+++ gpt2_karpathy/train_gpt2.py
@@ -173,24 +173,44 @@ print(f'using device: {device}')
 model = GPT(GPTConfig())
 model.to(device)
 
-# tokenize the text
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f:
-    text = f.read()
+# define the dataloader
 
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])
-buf = buf.to(device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(tokens)} in dataloader, num_batches in 1 epoch is {len(tokens) // (B * T)}")
+
+        self.current_position = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position: self.current_position + B * T + 1]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        else:
+            self.current_position += B * T
+        
+        return x, y
+
+dataloader = DataLoaderLite(4, 32)
 
 # optimizer loop
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
     optimizer.zero_grad()
+    x, y = dataloader.next_batch()
+    x, y = x.to(device), y.to(device)
     
     logits, loss = model(x, y)
     loss.backward()
