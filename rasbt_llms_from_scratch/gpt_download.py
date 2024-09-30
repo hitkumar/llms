5a982ecba3a8bc71c528f4cd4e28a22a6c69f377
hitkumar <hitesh.nahan@gmail.com>
2024-09-30 09:29:23 -0700

    [rasbt] adding dpo loss function

--- rasbt_llms_from_scratch/gpt_download.py
+++ rasbt_llms_from_scratch/gpt_download.py
@@ -15,6 +15,20 @@ from gpt_model import (
 )
 from tqdm import tqdm
 
+BASE_CONFIG = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 1024,  # Context length
+    "drop_rate": 0.0,  # Dropout rate
+    "qkv_bias": True,  # Query-key-value bias
+}
+
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
 
 def download_file(file_path, destination):
     response = requests.get(file_path, stream=True)
@@ -105,22 +119,7 @@ def load_gpt2(model_size, model_dir):
 
 if __name__ == "__main__":
     # Util to download the model.
-    BASE_CONFIG = {
-        "vocab_size": 50257,  # Vocabulary size
-        "context_length": 1024,  # Context length
-        "drop_rate": 0.0,  # Dropout rate
-        "qkv_bias": True,  # Query-key-value bias
-    }
-
-    model_configs = {
-        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
-        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
-        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
-        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
-    }
-
     CHOOSE_MODEL = "gpt2-medium (355M)"
-
     model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
     BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
     model_dir = os.path.join(
