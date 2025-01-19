287b19b523aee469f87e6b654ddbf2990884364c
hitkumar <hitesh.nahan@gmail.com>
2025-01-18 18:36:37 -0800

    [torchtitan] Datasets init make build_hf_data_loader available

--- torchtitan/core/datasets/__init__.py
+++ torchtitan/core/datasets/__init__.py
@@ -1,3 +1,4 @@
+from core.datasets.hf_datasets import build_hf_data_loader
 from core.datasets.tokenizer import build_tokenizer
 
-__all__ = ["build_tokenizer"]
+__all__ = ["build_tokenizer", "build_hf_data_loader"]
